c808ff97a070f52d85219c7e65757a1387afeda1
Jon Pugh <jon@thinkdrop.net>
2015-08-02 21:11:16 -0400

    Creating a page that will put a "terra status" command into the queue!

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\ORM\EntityManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use JMS\JobQueueBundle\Entity\Job;
 
 class DefaultController extends Controller
 {
@@ -17,5 +20,19 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
+    }
+
+    /**
+     * @Route("/command", name="queue command")
+     */
+    public function commandAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $job = new Job('status');
+        $em->persist($job);
+        $em->flush($job);
+
+        return new Response("Job persisted?");
+
     }
 }
